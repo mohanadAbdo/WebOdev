eec9828eba8702f9b459c67600e2ee54135d93be
Mohanad <Mohanadflorra@gmail.com>
2023-07-23 16:23:04 +0300

    Add project files.

---
+++ Udemy_1/Controllers/CategoryController.cs
@@ -0,0 +1,124 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Odev.Data;
+using Odev.Models;
+
+namespace Odev.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+           _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+        //GET
+        public IActionResult Create()
+        {
+            return View();
+        }
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString()) 
+                {
+                ModelState.AddModelError("name","Cant match The name");
+            
+            
+                }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                TempData["Success"] = "Category created";
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+
+
+
+        }  //GET
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0) {
+
+                return NotFound();
+            }
+            var categoryFromDb = _db.Categories.Find(id);
+            
+            if (categoryFromDb== null)
+            {
+                return NotFound();
+
+            }
+            return View(categoryFromDb);
+        }
+
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "Cant match The name");
+
+
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                TempData["Success"] = "Category edited";
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        //GET
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+
+                return NotFound();
+            }
+            var categoryFromDb = _db.Categories.Find(id);
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+
+            }
+            return View(categoryFromDb);
+        }
+
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            var obj = _db.Categories.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+         
+            
+                _db.Categories.Remove(obj);
+                _db.SaveChanges();
+            TempData["Success"] = "Category deleted";
+            return RedirectToAction("Index");
+            
+        }
+
+    }
+}
---
+++ Udemy_1/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Odev.Models;
+
+namespace Odev.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options) : base(options)
+        {
+            
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
